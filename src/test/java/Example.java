2e2e04a66368913925f4a248b510bfa3515cc292
joskyday <joskyday@gmail.com>
2022-06-16 22:30:16 +0300

    second commit

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,2 +1,7 @@
 public class Example {
 }
+// create the framework
+// add unrelated files into .gitignore
+// create example class under test
+// make first commit
+// after everything is ready push
