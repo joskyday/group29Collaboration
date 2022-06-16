33766e5a81242b976142870497284b23e09d68f8
joskyday <joskyday@gmail.com>
2022-06-16 22:56:51 +0300

    AC1 test3

--- src/test/java/AC1UserLogin.java
+++ src/test/java/AC1UserLogin.java
@@ -1,10 +1,17 @@
 public class AC1UserLogin {
     //@Test
-    public static void test1(){
+    public static void test1() {
 
     }
+
     //@Test
-    public static void test2(){
+    public static void test2() {
+
+    }
+
+    //@Test
+    public static void test3() {
+
 
     }
 }
